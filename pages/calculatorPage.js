67512323e2d36c952d785e7f29f18b5edfdf696a
Simas Markevičius <simas.markevicius@devbridge.com>
2021-08-11 20:15:44 +0300

    added homework tests

---
+++ pages/calculatorPage.js
@@ -0,0 +1,35 @@
+exports.calculatorPage = class duckpage {
+    constructor(page, build){
+        this.page = page;
+        this.build = build;
+    }
+    async goto()
+    {
+        await this.page.goto('https://testsheepnz.github.io/BasicCalculator ');
+        const dropdown = await this.page.$('#selectBuild');
+        await dropdown.selectOption([{'label':this.build}]);
+    }
+    async selectOperation(option)
+    {
+        const dropdown = await this.page.$('#selectOperationDropdown');
+        await dropdown.selectOption([{'label':option}]);
+    }
+    async calculate(value1, value2)
+    {
+        await this.page.fill('#number1Field', String(value1));
+        await this.page.fill('#number2Field', String(value2));
+        await this.page.click('#calculateButton');
+        //await this.page.waitForSelector('#numberAnswerField', {visible: false});
+        //await this.page.waitForSelector('#numberAnswerField', {visible: true});
+    }
+    async integerOnly(option)
+    {
+        if (option == true){
+            await this.page.check('#integerSelect');
+        }
+        else
+        { 
+            await this.page.uncheck('#integerSelect');
+        }
+    }
+}
